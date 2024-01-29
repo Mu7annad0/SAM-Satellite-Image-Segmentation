c3f9880441939a775f0efe5031aed264b2a70d68
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-01-29 02:35:36 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -73,7 +73,8 @@ def train_transform(img_size, orig_h, orig_w):
 
     transforms = []
     if orig_h < img_size and orig_w < img_size:
-        transforms.append(A.PadIfNeeded(min_height=img_size, min_width=img_size, border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0)))
+        transforms.append(A.PadIfNeeded(min_height=img_size, min_width=img_size, 
+                                        border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0)))
     else:
         transforms.append(A.Resize(int(img_size), int(img_size), interpolation=cv2.INTER_NEAREST))
 
@@ -84,7 +85,7 @@ def train_transform(img_size, orig_h, orig_w):
     return A.Compose(transforms, p=1.)
     
 
-# --------------------------loss functions--------------------------------------
+# -----------------------------loss functions--------------------------------------
 # Focal loss
 
 class Focal_loss(nn.Module):
@@ -151,4 +152,70 @@ class Focal_loss(nn.Module):
             loss = loss.sum()
         return loss
     
+
+# Dice Loss
     
+class DiceLoss(nn.Module):
+    def __init__(self, n_classes=1):
+        super(DiceLoss, self).__init__()
+        self.n_classes = n_classes
+
+    def _one_hot_encoder(self, input_tensor):
+        tensor_list = []
+        for i in range(self.n_classes):
+            temp_prob = input_tensor == i  # * torch.ones_like(input_tensor)
+            tensor_list.append(temp_prob.unsqueeze(1))
+        output_tensor = torch.cat(tensor_list, dim=1)
+        return output_tensor.float()
+
+    def _dice_loss(self, score, target):
+        target = target.float()
+        smooth = 1e-5
+        intersect = torch.sum(score * target)
+        y_sum = torch.sum(target * target)
+        z_sum = torch.sum(score * score)
+        loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+        loss = 1 - loss
+        return loss
+
+    def forward(self, inputs, target, weight=None, softmax=False):
+        if softmax:
+            inputs = torch.softmax(inputs, dim=1)
+        target = self._one_hot_encoder(target)
+        if weight is None:
+            weight = [1] * self.n_classes
+        assert inputs.size() == target.size(), 'predict {} & target {} shape do not match'.format(inputs.size(),
+                                                                                                  target.size())
+        class_wise_dice = []
+        loss = 0.0
+        for i in range(0, self.n_classes):
+            dice = self._dice_loss(inputs[:, i], target[:, i])
+            class_wise_dice.append(1.0 - dice.item())
+            loss += dice * weight[i]
+        return loss / self.n_classes
+
+
+# Intersection over Union (IoU) Loss
+
+class IoULoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(IoULoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #intersection is equivalent to True Positive count
+        #union is the mutually inclusive area of all labels & predictions 
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection 
+        
+        IoU = (intersection + smooth)/(union + smooth)
+                
+        return 1 - IoU
