3b292fa2547aea21bb15f3d8cb58eb6269789766
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-01-30 03:20:53 +0200

    Update DataLoader.py

--- DataLoader.py
+++ DataLoader.py
@@ -7,27 +7,34 @@ from PIL import Image
 from glob import glob
 from utils import get_bounding_box, visualize_img_mask_box, train_transform
 from cfg import parse_args
-import torchvision.transforms as Tr
 
 
 class RoadDataset(Dataset):
 
     def __init__(self, data_root, image_size = 256, train=True, box=True, transform=None):
+        """
+        Args:
+            data_root: The directory path where the dataset is stored or located.
+            image_size: Desired size for input images.
+            train: Flag indicating if the dataset is for training.
+            box: Indicates if bounding box information is included.
+            transform: Data transformations to be applied on the training dataset.
+        """
         self.data_root = data_root  
-        self.train  = train  # Indicates whether the dataset is used for training or not
-        self.transform = transform  # Transformation to apply to the images and masks
-        self.box = box  # Flag indicating whether bounding boxes should be included
-        self.image_size = image_size  # Size to which images will be resized
+        self.train  = train  
+        self.transform = transform  
+        self.box = box  
+        self.image_size = image_size 
 
-        # Initial transformation pipeline mostly used with test and valid datasets
+        # Initial transformation pipeline for mostley validation and test datset.
         self.initial_transform = A.Compose([
             A.Resize(image_size, image_size),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2()
         ])
-        # Retrieves image file names
+        # List of image file paths.
         self.img_names = sorted(glob(self.data_root + '/*_sat.jpg'))
-        # Keep track of transformed images
+        # Counter for transformed images.
         self.transformed_images = 0
 
     def __len__(self):
@@ -66,11 +73,17 @@ class RoadDataset(Dataset):
         # Condition to check if bounding boxes should be included
         if self.box is True:   
             boxes = get_bounding_box(mask)
-            return torch.tensor(image).float(), torch.tensor(mask).float(), torch.tensor(boxes).float()
-            # This line returns the image, mask, and bounding boxes as PyTorch tensors if the dataset includes bounding boxes.
-
-        # This line returns the image and mask as PyTorch tensors if the dataset does not include bounding boxes.
-        return torch.tensor(image).float(), torch.tensor(mask).float()
+            return {
+                'image': torch.tensor(image).float(), 
+                'mask': torch.tensor(mask).float(), 
+                'box': torch.tensor(boxes).float()}
+            # This returns the image, mask, and bounding boxes as PyTorch tensors if the dataset includes bounding boxes.
+        
+        # This returns the image and mask as PyTorch tensors if the dataset does not include bounding boxes.
+        return {
+            'image': torch.tensor(image).float(), 
+            'mask': torch.tensor(mask).float(), 
+        }
 
     def get_num_images(self):
         return self.transformed_images
@@ -84,13 +97,13 @@ if __name__ == '__main__':
     train_dataloader = DataLoader(dataset, 2, True)
 
     for i, batch in enumerate(train_dataloader):
-        image, mask, box = batch
-        image = image
-        mask = mask
-        break
+        image = batch['image']
+        mask = batch['mask']
+        if 'box' in batch:
+            box = batch['box']
 
     print(len(dataset))
     print(f'shape of image: {image.shape}')  # [B, 3, 256, 256]
     print(f'shape of mask: {mask.shape}')  # [B, 256, 256]
-    print(f'shape of box: {box.shape}')  #[B, 4]
+    print(f'shape of box: {box.shape}')  # [B, 4]
     visualize_img_mask_box(dataset, 3)
