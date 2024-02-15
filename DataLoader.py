ee3aafc35c9616a7a7d7301fa96446df5122a890
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-02-15 21:08:54 +0200

    Update DataLoader.py

--- DataLoader.py
+++ DataLoader.py
@@ -2,16 +2,35 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+import cv2
 import numpy as np
 from PIL import Image
 from glob import glob
-from utils import get_bounding_box, visualize_img_mask_box, train_transform
+from utils import get_bounding_box, visualize, init_point_sampling
 from cfg import parse_args
+import matplotlib.pyplot as plt
+
+
+def train_transform(img_size, orig_h, orig_w):
+
+    transforms = []
+    if orig_h < img_size and orig_w < img_size:
+        transforms.append(A.PadIfNeeded(min_height=img_size, min_width=img_size, 
+                                        border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0)))
+    else:
+        transforms.append(A.Resize(int(img_size), int(img_size), interpolation=cv2.INTER_NEAREST))
+
+    # transforms.append(A.HorizontalFlip(p=0.5))
+    # transforms.append(A.Rotate()),
+    transforms.append(A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
+    transforms.append(ToTensorV2(p=1.0))
+
+    return A.Compose(transforms, p=1.)
 
 
 class RoadDataset(Dataset):
 
-    def __init__(self, data_root, image_size = 256, train=True, box=True, transform=None):
+    def __init__(self, data_root, image_size = 256, train=True, box=True, points=None, transform=None):
         """
         Args:
             data_root: The directory path where the dataset is stored or located.
@@ -24,6 +43,7 @@ class RoadDataset(Dataset):
         self.train  = train  
         self.transform = transform  
         self.box = box  
+        self.points = points
         self.image_size = image_size 
 
         # Initial transformation pipeline for mostley validation and test datset.
@@ -48,7 +68,7 @@ class RoadDataset(Dataset):
         mask = Image.open(mask_file_name).convert('L')
 
         image, mask = np.asarray(image), np.asarray(mask)
-
+        mask = mask / 255
         h, w, _ = image.shape
 
         # Condition to check if the dataset is used for training
@@ -73,17 +93,23 @@ class RoadDataset(Dataset):
         # Condition to check if bounding boxes should be included
         if self.box is True:   
             boxes = get_bounding_box(mask)
-            return {
+            boxes = torch.tensor(boxes).float()
+        else:
+            boxes = None
+        
+        if self.points is True:
+            point_coords, point_labels = init_point_sampling(mask, get_point=8)
+        else:
+            point_coords = torch.zeros((0, 2))  # Empty tensor for coordinates
+            point_labels = torch.zeros(0, dtype=torch.int)  # Empty tensor for labels
+
+        return {
                 'image': torch.tensor(image).float(), 
                 'mask': torch.tensor(mask).float(), 
-                'box': torch.tensor(boxes).float()}
-            # This returns the image, mask, and bounding boxes as PyTorch tensors if the dataset includes bounding boxes.
-        
-        # This returns the image and mask as PyTorch tensors if the dataset does not include bounding boxes.
-        return {
-            'image': torch.tensor(image).float(), 
-            'mask': torch.tensor(mask).float(), 
-        }
+                'box': boxes,
+                'point_coords' : point_coords,
+                'point_labels' : point_labels
+            }
 
     def get_num_images(self):
         return self.transformed_images
@@ -93,17 +119,21 @@ if __name__ == '__main__':
       
     args = parse_args()
 
-    dataset = RoadDataset(args.train_root, 256, True, box = True, transform=True)
-    train_dataloader = DataLoader(dataset, 2, True)
+    dataset = RoadDataset(args.train_root, 512, True, box = True, points=True, transform=True)
+    train_dataloader = DataLoader(dataset, 5, True)
 
     for i, batch in enumerate(train_dataloader):
         image = batch['image']
         mask = batch['mask']
-        if 'box' in batch:
-            box = batch['box']
+        box = batch['box']
+        points_coords = batch['point_coords']
+        points_labels = batch['point_labels']
 
     print(len(dataset))
-    print(f'shape of image: {image.shape}')  # [B, 3, 256, 256]
-    print(f'shape of mask: {mask.shape}')  # [B, 256, 256]
+    print(f'shape of image: {image.shape}')  # [B, C, H, W]
+    print(f'shape of mask: {mask.shape}')  # [B, H, W]
     print(f'shape of box: {box.shape}')  # [B, 4]
-    visualize_img_mask_box(dataset, 3)
+    print(f'shape of point coors: {points_coords.shape}')  # [B, No_points, 2]
+    print(f'shape of point labels: {points_labels.shape}')  # [B, No_points]
+
+    visualize(dataset, 3)
