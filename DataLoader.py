72bf6e2a9009fe445eb334d21cc4680cb5f778de
MarioOsama <marioosamafayez@gmail.com>
2024-02-16 16:59:29 +0200

    Create BaseDataset class to be inherited by the dataset classes.

--- DataLoader.py
+++ DataLoader.py
@@ -12,10 +12,9 @@ import matplotlib.pyplot as plt
 
 
 def train_transform(img_size, orig_h, orig_w):
-
     transforms = []
     if orig_h < img_size and orig_w < img_size:
-        transforms.append(A.PadIfNeeded(min_height=img_size, min_width=img_size, 
+        transforms.append(A.PadIfNeeded(min_height=img_size, min_width=img_size,
                                         border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0)))
     else:
         transforms.append(A.Resize(int(img_size), int(img_size), interpolation=cv2.INTER_NEAREST))
@@ -28,52 +27,96 @@ def train_transform(img_size, orig_h, orig_w):
     return A.Compose(transforms, p=1.)
 
 
-class RoadDataset(Dataset):
-
-    def __init__(self, data_root, image_size = 256, train=True, box=True, points=None, transform=None):
+# Generic class to be inherited by the dataset classes
+class BaseDataset(Dataset):
+    def __init__(self, data_root: str, image_size: int = 256, is_train: bool = True, is_box: bool = True,
+                 is_points: bool = False, is_transform: bool = False):
         """
         Args:
             data_root: The directory path where the dataset is stored or located.
             image_size: Desired size for input images.
-            train: Flag indicating if the dataset is for training.
-            box: Indicates if bounding box information is included.
-            transform: Data transformations to be applied on the training dataset.
+            is_train: Flag indicating if the dataset is for training.
+            is_box: Indicates if bounding box information is included.
+            is_points: Indicates if point information is included.
+            is_transform: Data transformations to be applied on the training dataset.
         """
-        self.data_root = data_root  
-        self.train  = train  
-        self.transform = transform  
-        self.box = box  
-        self.points = points
-        self.image_size = image_size 
+        self.data_root = data_root
+        self.image_size = image_size
+        self.is_train = is_train
+        self.is_transform = is_transform
+        self.is_box = is_box
+        self.is_points = is_points
 
-        # Initial transformation pipeline for mostley validation and test datset.
         self.initial_transform = A.Compose([
             A.Resize(image_size, image_size),
             A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ToTensorV2()
         ])
-        # List of image file paths.
-        self.img_names = sorted(glob(self.data_root + '/*_sat.jpg'))
-        # Counter for transformed images.
+
         self.transformed_images = 0
+        # List of image file paths.
+        self.image_paths = self.list_image_files()
+        self.mask_paths = self.list_mask_files()
+
+    def list_image_files(self) -> list[str]:
+        """
+        This method should be implemented by subclasses of BaseDataset class to list the image files in the dataset.
+        """
+        raise NotImplementedError("Subclasses of BaseDataset should implement list_image_files.")
+
+    def list_mask_files(self) -> list[str]:
+        """
+        This method should be implemented by subclasses of BaseDataset class to list the mask files in the dataset.
+        """
+        raise NotImplementedError("Subclasses of BaseDataset should implement list_mask_files.")
+
+    def get_num_transformed_images(self) -> object:
+        """
+        This method returns the number of transformed images in the dataset.
+        """
+        return self.transformed_images
+
+    def __getitem__(self, index, image_read_mode: str = 'RGB', mask_read_mode: str = 'L') -> tuple[
+        np.ndarray, np.ndarray]:
+        """
+        This method returns the image and mask at the specified index.
+        """
+        img_file_path = self.image_paths[index]
+        mask_file_path = self.mask_paths[index]
+
+        image_file = Image.open(img_file_path).convert(image_read_mode)
+        mask_file = Image.open(mask_file_path).convert(mask_read_mode)
+
+        np_image, np_mask = np.asarray(image_file), np.asarray(mask_file)
+        return np_image, np_mask
+
+    def process_image(self, index: int) -> dict:
+        """
+        This method should be implemented by subclasses of BaseDataset class to processes the image and mask.
+        """
+        raise NotImplementedError("Subclasses of BaseDataset should implement process_image.")
 
     def __len__(self):
-        return len(self.img_names)
+        """
+        This method returns the length of the dataset.
+        """
+        return len(self.image_paths)
 
-    def __getitem__(self, index):
-        img_file_path = self.img_names[index]
-        mask_file_name = img_file_path.replace('.jpg', '.png').replace('sat', 'mask')
 
-        image = Image.open(img_file_path).convert('RGB')
-        mask = Image.open(mask_file_name).convert('L')
+class RoadDataset(BaseDataset):
+    def list_image_files(self):
+        return sorted(glob(self.data_root + '/*_sat.jpg'))
 
-        image, mask = np.asarray(image), np.asarray(mask)
+    def list_mask_files(self):
+        return sorted(glob(self.data_root + '/*_mask.png'))
+
+    def process_image(self, index) -> dict:
+        image, mask = self.__getitem__(index)
         mask = mask / 255
         h, w, _ = image.shape
 
-        # Condition to check if the dataset is used for training
-        if self.train:
-            if self.transform is True:
+        if self.is_train:
+            if self.is_transform:
                 # Obtain training transformation
                 transformer = train_transform(self.image_size, h, w)
                 augmented = transformer(image=image, mask=mask)
@@ -91,35 +134,31 @@ class RoadDataset(Dataset):
             self.transformed_images += 1
 
         # Condition to check if bounding boxes should be included
-        if self.box is True:   
+        if self.is_box:
             boxes = get_bounding_box(mask)
             boxes = torch.tensor(boxes).float()
         else:
             boxes = None
-        
-        if self.points is True:
+
+        if self.is_points:
             point_coords, point_labels = init_point_sampling(mask, get_point=8)
         else:
             point_coords = torch.zeros((0, 2))  # Empty tensor for coordinates
             point_labels = torch.zeros(0, dtype=torch.int)  # Empty tensor for labels
 
         return {
-                'image': torch.tensor(image).float(), 
-                'mask': torch.tensor(mask).float(), 
-                'box': boxes,
-                'point_coords' : point_coords,
-                'point_labels' : point_labels
-            }
-
-    def get_num_images(self):
-        return self.transformed_images
+            'image': torch.tensor(image).float(),
+            'mask': torch.tensor(mask).float(),
+            'box': boxes,
+            'point_coords': point_coords,
+            'point_labels': point_labels
+        }
 
 
 if __name__ == '__main__':
-      
     args = parse_args()
 
-    dataset = RoadDataset(args.train_root, 512, True, box = True, points=True, transform=True)
+    dataset = BaseDataset(args.train_root, 512, True, is_box=True, is_points=True, is_transform=True)
     train_dataloader = DataLoader(dataset, 5, True)
 
     for i, batch in enumerate(train_dataloader):
