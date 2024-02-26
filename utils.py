f1d07c500dc08e11369f1ce8fbd63eeec1cd682f
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-02-26 12:09:36 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,14 +17,14 @@ def get_bounding_box(ground_truth_map):
 
     # Check if there are non-zero values in the mask
     if np.count_nonzero(ground_truth_array) == 0:
-          # If there are no non-zero values, return a default bounding box or handle it as needed
-          return [0, 0, 1, 1]
-    
+        # If there are no non-zero values, return a default bounding box or handle it as needed
+        return [0, 0, 1, 1]
+
     # get bounding box from mask
     y_indices, x_indices = np.where(ground_truth_array > 0)
     x_min, x_max = np.min(x_indices), np.max(x_indices)
     y_min, y_max = np.min(y_indices), np.max(y_indices)
-    
+
     # add perturbation to bounding box coordinates
     H, W = ground_truth_array.shape
     x_min = max(0, x_min - np.random.randint(0, 20))
@@ -48,10 +48,10 @@ def init_point_sampling(mask, get_point=1):
     """
     if isinstance(mask, torch.Tensor):
         mask = mask.numpy()
-        
-     # Get coordinates of black/white pixels
-    fg_coords = np.argwhere(mask == 1)[:,::-1]
-    bg_coords = np.argwhere(mask == 0)[:,::-1]
+
+    # Get coordinates of black/white pixels
+    fg_coords = np.argwhere(mask == 1)[:, ::-1]
+    bg_coords = np.argwhere(mask == 0)[:, ::-1]
 
     fg_size = len(fg_coords)
     bg_size = len(bg_coords)
@@ -66,7 +66,7 @@ def init_point_sampling(mask, get_point=1):
             fg_coord = bg_coords[index]
             label = 0
         return torch.as_tensor([fg_coord.tolist()], dtype=torch.float), torch.as_tensor([label], dtype=torch.int)
-    
+
     else:
         num_fg = get_point // 2
         num_bg = get_point - num_fg
@@ -77,60 +77,65 @@ def init_point_sampling(mask, get_point=1):
         coords = np.concatenate([fg_coords, bg_coords], axis=0)
         labels = np.concatenate([np.ones(num_fg), np.zeros(num_bg)]).astype(int)
         indices = np.random.permutation(get_point)
-        coords, labels = torch.as_tensor(coords[indices], dtype=torch.float), torch.as_tensor(labels[indices], dtype=torch.int)
+        coords, labels = torch.as_tensor(coords[indices], dtype=torch.float), torch.as_tensor(labels[indices],
+                                                                                              dtype=torch.int)
         return coords, labels
-    
+
 
 def transformation(img_size, orig_h, orig_w, train=True):
+    transforms = []
+    if orig_h < img_size and orig_w < img_size:
+        transforms.append(A.PadIfNeeded(min_height=img_size, min_width=img_size,
+                                        border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0)))
+    else:
+        transforms.append(A.Resize(int(img_size), int(img_size), interpolation=cv2.INTER_NEAREST))
 
-        transforms = []
-        if orig_h < img_size and orig_w < img_size:
-            transforms.append(A.PadIfNeeded(min_height=img_size, min_width=img_size, 
-                                            border_mode=cv2.BORDER_CONSTANT, value=(0, 0, 0)))
-        else:
-            transforms.append(A.Resize(int(img_size), int(img_size), interpolation=cv2.INTER_NEAREST))
+    if train:
+        transforms.append(A.HorizontalFlip(p=0.5))
+        transforms.append(A.Rotate()),
 
-        if train:
-            transforms.append(A.HorizontalFlip(p=0.5))
-            transforms.append(A.Rotate()),
-        
-        transforms.append(A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
-        transforms.append(ToTensorV2(p=1.0))
+    transforms.append(A.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
+    transforms.append(ToTensorV2(p=1.0))
+
+    return A.Compose(transforms, p=1.)
 
 
-        return A.Compose(transforms, p=1.)
 # ------------------------------------------visualization-------------------------------------------------
-    
+
 
 def show_mask(mask, ax, random_color=False):
     if random_color:
         color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
     else:
-        color = np.array([0/255, 0/255, 0/255, 0.6])
+        color = np.array([0 / 255, 0 / 255, 0 / 255, .6])
     h, w = mask.shape[-2:]
     mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
     ax.imshow(mask_image)
-    
+
+
 def show_points(coords, labels, ax, marker_size=375):
-    pos_points = coords[labels==1]
-    neg_points = coords[labels==0]
-    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='#57d459', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)   
-    
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='#57d459', marker='*', s=marker_size, edgecolor='white',
+               linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white',
+               linewidth=1.25)
+
+
 def show_box(box, ax):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='#FF8000', facecolor=(0,0,0,0), lw=2))    
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='#FF8000', facecolor=(0, 0, 0, 0), lw=2))
 
 
-def visualize_from_path(image_path, mask_path, box = True, points = True):
+def visualize_from_path(image_path, mask_path, box=True, points=True):
     plt.figure(figsize=(10, 10))
     image = Image.open(image_path).convert('RGB')
     image = np.array(image)
     mask = Image.open(mask_path).convert('L')
     mask = np.array(mask)
     if mask.max() == 255:
-            mask = mask / 255
+        mask = mask / 255
 
     plt.imshow(image)
     show_mask(mask, plt.gca())
@@ -141,22 +146,21 @@ def visualize_from_path(image_path, mask_path, box = True, points = True):
     if points:
         points_coords, point_labels = init_point_sampling(mask, 8)
         point_coords = np.array(points_coords)
-        point_labels = np.array(point_labels)    
-        show_points(point_coords, point_labels, plt.gca())  
-    
+        point_labels = np.array(point_labels)
+        show_points(point_coords, point_labels, plt.gca())
+
     plt.axis('on')
-    plt.show()  
-       
-       
+    plt.show()
+
+
 def visualize(dataloader, num_images):
-    
     for batch in dataloader:
         images = batch['image']
         masks = batch['mask']
         box = batch['box']
         points_coords = batch['point_coords']
         point_labels = batch['point_labels']
-        
+
         # Iterate over images and masks in the batch
         for image, mask, box, points_coords, point_labels in zip(images, masks, box, points_coords, point_labels):
             image_pil = to_pil_image(image)
@@ -166,16 +170,17 @@ def visualize(dataloader, num_images):
             show_mask(mask, plt.gca())
             show_box(box, plt.gca())
             point_coords = np.array(points_coords)
-            point_labels = np.array(point_labels)  
-            show_points(point_coords, point_labels, plt.gca())  
+            point_labels = np.array(point_labels)
+            show_points(point_coords, point_labels, plt.gca())
             plt.axis('on')
-            plt.show()  
+            plt.show()
             num_images -= 1  # Decrement the count of images
             if num_images == 0:
-                return 
+                return
+
+            # ------------------------------------------loss functions------------------------------------------------
 
 
-# ------------------------------------------loss functions------------------------------------------------
 # Focal loss
 
 class FocalLoss(nn.Module):
@@ -202,10 +207,10 @@ class FocalLoss(nn.Module):
         loss = (torch.sum(loss_pos) + torch.sum(loss_neg)) / (num_pos + num_neg + 1e-12)
 
         return loss
-    
+
 
 # Dice Loss
-    
+
 class DiceLoss(nn.Module):
     def __init__(self, smooth=1.0):
         super(DiceLoss, self).__init__()
@@ -232,24 +237,23 @@ class IoULoss(nn.Module):
         super(IoULoss, self).__init__()
 
     def forward(self, inputs, targets, smooth=1):
-        
-        #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = F.sigmoid(inputs)       
-        
-        #flatten label and prediction tensors
+        # comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)
+
+        # flatten label and prediction tensors
         inputs = inputs.view(-1)
         targets = targets.view(-1)
-        
-        #intersection is equivalent to True Positive count
-        #union is the mutually inclusive area of all labels & predictions 
+
+        # intersection is equivalent to True Positive count
+        # union is the mutually inclusive area of all labels & predictions
         intersection = (inputs * targets).sum()
         total = (inputs + targets).sum()
-        union = total - intersection 
-        
-        IoU = (intersection + smooth)/(union + smooth)
-                
+        union = total - intersection
+
+        IoU = (intersection + smooth) / (union + smooth)
+
         return 1 - IoU
-       
+
 
 class MaskIoULoss(nn.Module):
 
@@ -270,12 +274,11 @@ class MaskIoULoss(nn.Module):
         iou = (intersection + 1e-7) / (union + 1e-7)
         iou_loss = torch.mean((iou - pred_iou) ** 2)
         return iou_loss
-    
 
 
 # FocalDiceloss_IoULoss
 class FocalDiceloss_IoULoss(nn.Module):
-    
+
     def __init__(self, weight=20.0, iou_scale=1.0):
         super(FocalDiceloss_IoULoss, self).__init__()
         self.weight = weight
@@ -292,12 +295,11 @@ class FocalDiceloss_IoULoss(nn.Module):
         assert pred.shape == mask.shape, "pred and mask should have the same shape."
 
         focal_loss = self.focal_loss(pred, mask)
-        dice_loss =self.dice_loss(pred, mask)
+        dice_loss = self.dice_loss(pred, mask)
         loss1 = self.weight * focal_loss + dice_loss
         loss2 = self.maskiou_loss(pred, mask, pred_iou)
         loss = loss1 + loss2 * self.iou_scale
         return loss
-
 
 
 # -------------------------------------------Metrics------------------------------------------------
@@ -323,26 +325,26 @@ def _list_tensor(x, y):
     return x, y
 
 
-def iou(pr, gt, eps=1e-7, threshold = 0.5):
+def iou(pr, gt, eps=1e-7, threshold=0.5):
     pr_, gt_ = _list_tensor(pr, gt)
     pr_ = _threshold(pr_, threshold=threshold)
     gt_ = _threshold(gt_, threshold=threshold)
-    intersection = torch.sum(gt_ * pr_,dim=[1,2,3])
-    union = torch.sum(gt_,dim=[1,2,3]) + torch.sum(pr_,dim=[1,2,3]) - intersection
+    intersection = torch.sum(gt_ * pr_, dim=[1, 2, 3])
+    union = torch.sum(gt_, dim=[1, 2, 3]) + torch.sum(pr_, dim=[1, 2, 3]) - intersection
     return ((intersection + eps) / (union + eps)).cpu().numpy()
 
 
-def dice(pr, gt, eps=1e-7, threshold = 0.5):
+def dice(pr, gt, eps=1e-7, threshold=0.5):
     pr_, gt_ = _list_tensor(pr, gt)
     pr_ = _threshold(pr_, threshold=threshold)
     gt_ = _threshold(gt_, threshold=threshold)
-    intersection = torch.sum(gt_ * pr_,dim=[1,2,3])
-    union = torch.sum(gt_,dim=[1,2,3]) + torch.sum(pr_,dim=[1,2,3])
-    return ((2. * intersection +eps) / (union + eps)).cpu().numpy()
+    intersection = torch.sum(gt_ * pr_, dim=[1, 2, 3])
+    union = torch.sum(gt_, dim=[1, 2, 3]) + torch.sum(pr_, dim=[1, 2, 3])
+    return ((2. * intersection + eps) / (union + eps)).cpu().numpy()
 
 
 def SegMetrics(pred, label, metrics):
-    metric_list = []  
+    metric_list = []
     if isinstance(metrics, str):
         metrics = [metrics, ]
     for i, metric in enumerate(metrics):
