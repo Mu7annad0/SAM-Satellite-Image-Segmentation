3988275cc55787286df7e3b8ced577554d99a458
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-03-01 00:02:31 +0200

    Early stopping class

--- utils.py
+++ utils.py
@@ -178,7 +178,8 @@ def visualize(dataloader, num_images):
             if num_images == 0:
                 return
 
-            # ------------------------------------------loss functions------------------------------------------------
+
+# ------------------------------------------loss functions------------------------------------------------
 
 
 # Focal loss
@@ -361,3 +362,36 @@ def SegMetrics(pred, label, metrics):
     else:
         raise ValueError('metric mistakes in calculations')
     return metric
+
+
+class EarlyStopping:
+
+    def __init__(self, patience=7, delta=0):
+        """
+        Early stops the training if validation loss doesn't improve after a given patience.
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+        """
+        self.patience = patience
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.delta = delta
+
+    def __call__(self, val_loss):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.counter = 0
