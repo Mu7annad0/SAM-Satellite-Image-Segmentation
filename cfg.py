e4b33f3e3b98b0608b50e1898c22b80520e5b2ed
Muhannad Ashraf <mu7nnd.as@gmail.com>
2024-03-18 21:39:35 +0200

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -5,6 +5,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--num_epochs", type=int, default=20)
+    parser.add_argument("--net", default="sam", type=str)
     parser.add_argument("--img_size", type=int, default=512)
     parser.add_argument("--train_root", type=str,
                         default="../Dataset/DeepGlobeRoadExtraction/road/train/")
@@ -12,7 +13,11 @@ def parse_args():
                         default="../Dataset/DeepGlobeRoadExtraction/road/valid/")
     parser.add_argument("--test_root", type=str,
                         default="../Dataset/DeepGlobeRoadExtraction/road/test/")
-    parser.add_argument("--dubai_train_root", type=str, default="../Dataset/Semantic segmentation dataset/train/*/")
+    parser.add_argument("--xd_train_root", type=str, default="../Dataset/XD/train/")
+    parser.add_argument("--xd_valid_root", type=str, default="../Dataset/XD/valid/")
+    parser.add_argument("--aerial_train_root", type=str, default="../Dataset/aerial/train/")
+    parser.add_argument("--aerial_valid_root", type=str, default="../Dataset/aerial/valid/")
+    parser.add_argument("--aerial_test_root", type=str, default="../Dataset/aerial/test/")
     parser.add_argument("--dubai_valid_root", type=str, default="../Dataset/Semantic segmentation dataset/valid/*/")
     parser.add_argument('--device', type=str, default="mps")
     parser.add_argument('--batch_size', type=int, default=16)
@@ -28,9 +33,12 @@ def parse_args():
     parser.add_argument('--box', type=bool, default=True, help="using box as prompt")
     parser.add_argument('--point', type=bool, default=True, help="using points as prompt")
     parser.add_argument('--use_adapter', type=bool, default=False, help="using adpter in the training")
-    parser.add_argument("--point_list", type=list, default=[1, 3, 5, 9], help="point_list")
+    parser.add_argument("--point_list", type=list, default=[5, 10, 15, 20], help="point_list")
+    parser.add_argument("--point_iterator", type=list, default=20, help="points iteration")
     parser.add_argument("--use_scheduler", type=bool, default=True, help="use scheduler")
     parser.add_argument("--early_stop", type=bool, default=True, help="early stop the training process")
-
+    parser.add_argument("--seed", type=int, default=2049, help="random seed factor")
+    parser.add_argument("--resume_training", type=str, default=None, help="resume training") 
+    parser.add_argument("--multimask", type=bool, default=False, help="ouput multimask")
     args = parser.parse_args()
     return args
